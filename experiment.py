a908b38d9a67b1240d2f4f73b628090024f88e30
khhandrea <khhandrea@kakao.com>
2023-10-19 21:53:56 +0900

    feat Initialize

---
+++ experiment.py
@@ -0,0 +1,23 @@
+import os
+
+if __name__ == '__main__':
+    # 각 실험: 돌리고 data 저장 (main이면 data 저장 안 하고 바로 출력)
+
+    # DP experiment (until convergence)
+    # MC experiment with Ne
+    # N-step TD experiment with Ne
+
+    # print V(s) for all
+    # print errors and their mean, variance
+
+    # plot errors with mean average for 100 steps with data
+    # plot errors with mean average for 1000 steps with data
+    # plot errors with mean average for 10000 steps with data
+    # plot errors with mean average for 100000 steps with data
+
+    # data: 첫줄엔 최종 V값, 두번째 줄엔 step별 loss (띄어쓰기로 구분)
+    # (experiment)-(step).txt
+    # experiment: DP, MC, TD 1step, TD 3step
+    # step: 100, 1000, 10000, 100000
+    # 경우의수 총 16개
+    print('hi')
